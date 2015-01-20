b239d46dacde8334abe873bd4c3185988093d259
Lars Emil Knudsen <larsemil87@gmail.com>
2015-01-20 08:44:09 +0100

    Lagt til skall til eksempler til forelesning 4 - Layouts

---
+++ Forelesning04/PlayingWithLayouts/app/src/main/java/com/capgemini/larseknu/playingwithlayouts/HierarchyViewerActivity.java
@@ -0,0 +1,15 @@
+package com.capgemini.larseknu.playingwithlayouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class HierarchyViewerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout);
+    }
+
+}
---
+++ Forelesning04/PlayingWithLayouts/app/src/main/java/com/capgemini/larseknu/playingwithlayouts/LinearLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.capgemini.larseknu.playingwithlayouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class LinearLayoutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+
+}
